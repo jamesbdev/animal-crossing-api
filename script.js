aadb54ad744d772aa78bdbd2fcab7ec383c838be
James Bremner <j.bremner@ymail.com>
2024-10-14 15:31:01 +0100

    complete fetch request

--- script.js
+++ script.js
@@ -3,18 +3,32 @@ const apiKey = "c75ac129-2650-4330-b9b4-5f37a7df4330";
 
 //fetch data about single random Animal Crossing villager
 
+//function to create a random number
+const makeRandNumber = (max) => {
+  return Math.floor(Math.random() * max);
+}
+
+
+//gets data of random villager
 const getRandomVillager = async () => {
     //select a random number 
     const url = "https://api.nookipedia.com/villagers";
 
     try {
-        const response = await fetch(url);
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'X-API-KEY': apiKey,
+                'Accepted-Version': "1.0.0",
+            }
+        });
         if (!response.ok) {
             throw new Error(`Response status: ${response.status}`);
         }
 
         const json = await response.json();
-        console.log(json);
+        const villagerNumber = makeRandNumber(json.length);
+        console.log(json[villagerNumber]);
       
     } catch (error) {
       console.log(error.message);

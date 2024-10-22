cb1fafed22c19f523e762a54303a9f374ebbb67d
James Bremner <j.bremner@ymail.com>
2024-10-22 15:27:08 +0100

    check for quote

--- script.js
+++ script.js
@@ -65,8 +65,11 @@ const getRandomVillager = async () => {
             villagerImage.classList.add("border-pink");
         }
    
-        //change quote 
-        quote.innerHTML = villager.quote;
+       
+        if (villager.quote) {
+          //change quote 
+          quote.innerHTML = villager.quote;
+        }
         //change name
         name.innerHTML = villager.name;
         //change species
@@ -76,14 +79,26 @@ const getRandomVillager = async () => {
         //change hobby
         if (villager.hobby) {
           hobby.innerHTML = villager.hobby;
+          //change hobby icon according to data
+          const hobbyImg = document.querySelector(".hobby-image");
+          hobbyImg.src = `/assets/${villager.hobby}.svg`;
+          
         } else {
             console.log(villager.hobby);
             const hobbyContainer = document.querySelector(".hobby-container");
             //hide hobby section if there are no hobbies
             hobbyContainer.style.display = "none";
         }
-        //change birthday
-        birthday.insertAdjacentElement("beforeend", birthdayInfo);
+      
+
+        if (!villager.birthday_day || !villager.birthday_month) {
+            //hide the birthday container if there is no birthday information
+            const birthdayContainer = document.querySelector(".birthday-container"); 
+            birthdayContainer.style.display = "none";
+        } else {
+          //change birthday
+          birthday.insertAdjacentElement("beforeend", birthdayInfo);
+        }
 
         //change title colour
         if (textColor && titleColor) {
